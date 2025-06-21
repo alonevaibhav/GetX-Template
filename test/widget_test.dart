b95a88d10f09daf1ac149e5626a0c169e440d64f
alonevaibhav <91304172+alonevaibhav@users.noreply.github.com>
2025-06-21 14:31:51 +0530

    Added Translation Service

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:nomore/main.dart';
+import 'package:setuapp/main.dart';
 
 
 void main() {
